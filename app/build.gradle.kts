9f17283290a4187494e7fad7ecd27e0df3d55ec7
Mimo Developer <mimo.gst@gmail.com>
2026-07-17 06:56:22 +0000

    Fix build: exclude META-INF/INDEX.LIST for Google Drive dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ android {
             excludes += "/META-INF/LICENSE.txt"
             excludes += "/META-INF/NOTICE"
             excludes += "/META-INF/NOTICE.txt"
+            excludes += "/META-INF/INDEX.LIST"
+            excludes += "/META-INF/INDEX.md"
         }
     }
 }
